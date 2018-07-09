613a8b16d9eac54ffbfad7080a2ccfd775250f56
Luis Angel Romero Escalona <angel.romero.es93@gmail.com>
2018-07-09 17:28:31 -0500

    Ajustes

---
+++ platforms/browser/www/js/index.js
@@ -0,0 +1,86 @@
+
+$(document).ready(function(){
+
+
+   $(".usuario").text(localStorage.getItem("nombre"));
+
+$(".credit").text("$"+localStorage.getItem("credit"));
+   
+                       
+
+    
+   $(".close").click(function(){
+   	       localStorage.clear();
+   	       $.mobile.navigate( "#login", {transition:"pop", info: "info about the #foo hash" });
+   });
+  
+});
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        console.log('Received Device Ready Event');
+        console.log('calling setup push');
+        app.setupPush();
+    },
+    setupPush: function() {
+        console.log('calling push init');
+        var push = PushNotification.init({
+            "android": {
+                "senderID": "850130729204",
+                "sound": true,
+                "vibrate": true,
+                "forceShow":true
+            },
+            "browser": {},
+            "ios": {
+                "sound": true,
+                "vibration": true,
+                "badge": true
+            },
+            "windows": {}
+        });
+        console.log('after init');
+
+        push.on('registration', function(data) {
+            console.log('registration event: ' + data.registrationId);
+            
+            var oldRegId = localStorage.getItem('registrationId');
+            
+                // Save new registration ID
+                localStorage.setItem('registrationId', data.registrationId);
+                // Post registrationId to your app server as the value has changed
+            
+
+          
+        });
+
+        push.on('error', function(e) {
+            console.log("push error = " + e.message);
+        });
+
+        push.on('notification', function(data) {
+            console.log('notification event');
+            navigator.notification.alert(
+                data.message,         // message
+                null,                 // callback
+                data.title,           // title
+                'Oki'                  // buttonName
+            );
+       });
+    }
+};
+app.initialize();
